155b5b1acecd00cafc6fc6335d7f023530caa794
Rafael Kitover <rkitover@gmail.com>
2022-02-15 08:15:55 +0000

    build: support VCPKG_ROOT env in vcpkg_install.bat
    If %VCPKG_ROOT% environment variable is set, check that the parent
    directory exists and throw an error if it does not.
    
    If %VCPKG_ROOT% is not set, set it to %cwd%\vcpkg, which was the
    previous behavior.
    
    Change all references to %cwd%\vcpkg to %VCPKG_ROOT%.
    
    Change the documentation in the top comment to refer to %VCPKG_ROOT%.
    
    This allows a developer to maintain his or her own vcpkg clone and not
    have to unnecessarily repeat the very time-consuming process of
    compiling the dependencies.
    
    Set ENV{VCPKG_ROOT} to the VCPKG_DIR default in CMakeLists.txt, because
    the cmake code does not support this yet, this is introduced in a
    subsequent commit.
    
    Signed-off-by: Rafael Kitover <rkitover@gmail.com>

--- compat/vcbuild/vcpkg_install.bat
+++ compat/vcbuild/vcpkg_install.bat
@@ -4,25 +4,26 @@ REM This script installs the "vcpkg" source package manager and uses
 REM it to build the third-party libraries that git requires when it
 REM is built using MSVC.
 REM
-REM [1] Install VCPKG.
-REM     [a] Create <root>/compat/vcbuild/vcpkg/
+REM [1] Install VCPKG (unless already exists.)
+REM     [a] Create %VCPKG_ROOT% defaulting to
+REM         <root>/compat/vcbuild/vcpkg/.
 REM     [b] Download "vcpkg".
 REM     [c] Compile using the currently installed version of VS.
-REM     [d] Create <root>/compat/vcbuild/vcpkg/vcpkg.exe
+REM     [d] Create %VCPKG_ROOT%/vcpkg.exe
 REM
-REM [2] Install third-party libraries.
+REM [2] Install third-party libraries (unless already installed.)
 REM     [a] Download each (which may also install CMAKE).
 REM     [b] Compile in RELEASE mode and install in:
-REM         vcpkg/installed/<arch>/{bin,lib}
+REM         %VCPKG_ROOT%/installed/<arch>/{bin,lib}
 REM     [c] Compile in DEBUG mode and install in:
-REM         vcpkg/installed/<arch>/debug/{bin,lib}
+REM         %VCPKG_ROOT%/installed/<arch>/debug/{bin,lib}
 REM     [d] Install headers in:
-REM         vcpkg/installed/<arch>/include
+REM         %VCPKG_ROOT%/installed/<arch>/include
 REM
 REM [3] Create a set of MAKE definitions for the top-level
 REM     Makefile to allow "make MSVC=1" to find the above
 REM     third-party libraries.
-REM     [a] Write vcpkg/VCPGK-DEFS
+REM     [a] Write %VCPKG_ROOT%/VCPGK-DEFS
 REM
 REM https://blogs.msdn.microsoft.com/vcblog/2016/09/19/vcpkg-a-tool-to-acquire-and-build-c-open-source-libraries-on-windows/
 REM https://github.com/Microsoft/vcpkg
@@ -40,7 +41,14 @@ REM ================================================================
 	@FOR /F "delims=" %%D IN ("%~dp0") DO @SET cwd=%%~fD
 	cd %cwd%
 
-	IF EXIST vcpkg\vcpkg.exe goto :install_libraries
+	IF NOT DEFINED VCPKG_ROOT (
+		set VCPKG_ROOT=%cwd%\vcpkg
+	) ELSE (IF NOT EXIST %VCPKG_ROOT%\..\ (
+		echo Invalid VCPKG_ROOT: %VCPKG_ROOT%, not under a valid directory. >&2
+		exit /B 1
+	))
+
+	IF EXIST %VCPKG_ROOT%\vcpkg.exe goto :install_libraries
 
 	git.exe version 2>nul
 	IF ERRORLEVEL 1 (
@@ -50,29 +58,29 @@ REM ================================================================
 		EXIT /B 1
 	)
 
-	echo Fetching vcpkg in %cwd%vcpkg
-	git.exe clone https://github.com/Microsoft/vcpkg vcpkg
+	echo Fetching vcpkg in %VCPKG_ROOT%
+	git.exe clone https://github.com/Microsoft/vcpkg %VCPKG_ROOT%
 	IF ERRORLEVEL 1 ( EXIT /B 1 )
 
-	cd vcpkg
+	cd %VCPKG_ROOT%
 	echo Building vcpkg
 	powershell -exec bypass scripts\bootstrap.ps1
 	IF ERRORLEVEL 1 ( EXIT /B 1 )
 
-	echo Successfully installed %cwd%vcpkg\vcpkg.exe
+	echo Successfully installed %VCPKG_ROOT%\vcpkg.exe
 
 :install_libraries
 
 	echo Installing third-party libraries(%arch%)...
 	FOR %%i IN (zlib expat libiconv openssl libssh2 curl) DO (
-	    cd %cwd%vcpkg
+	    cd %VCPKG_ROOT%
 	    IF NOT EXIST "packages\%%i_%arch%" CALL :sub__install_one %%i
 	    IF ERRORLEVEL 1 ( EXIT /B 1 )
 	)
 
 :install_defines
 	cd %cwd%
-	SET inst=%cwd%vcpkg\installed\%arch%
+	SET inst=%VCPKG_ROOT%\installed\%arch%
 
 	echo vcpkg_inc=-I"%inst%\include">VCPKG-DEFS
 	echo vcpkg_rel_lib=-L"%inst%\lib">>VCPKG-DEFS
